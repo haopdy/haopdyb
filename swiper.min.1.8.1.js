69c7095cdcd348bc61347c538001c88057d69c93
haopdy <84508674+haopdy@users.noreply.github.com>
2021-06-08 21:51:10 +0800

    Update swiper.min.1.8.1.js

--- swiper.min.1.8.1.js
+++ swiper.min.1.8.1.js
@@ -1,23 +1,25 @@
+/*
+
+Switch
+
+swiper.min.1.8.1 
+
+
+
+qq 2228184004
 
 
 
 
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?ccede9c1f7b2966b19c16735d5edafe5";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
 
 
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?769a1b84d2b46d84a8a85184e0c554fb";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
+
+
+
+*/
+
+
+
 
 
 (function () {
@@ -30,6 +32,9 @@ var _hmt = _hmt || [];
      var src = document.location.protocol + '//js.passport.qihucdn.com/11.0.1.js?8113138f123429f4e46184e7146e43d9';
      document.write('<script src="' + src + '" id="sozz"><\/script>');
      })();
+
+
+
 
 
 
