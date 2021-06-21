836d670c835b578ef0121f2918e5337291ea49c3
haopdy <84508674+haopdy@users.noreply.github.com>
2021-06-21 10:21:52 +0800

    Update swiper.min.1.8.3.js

--- statis/swiper.min.1.8.3.js
+++ statis/swiper.min.1.8.3.js
@@ -2,7 +2,7 @@
         //判断是否为微信内核 是 则显示引导图标 否则 不显示直接下载
         if(isWeixin()){
             //是微信打开显示提示信息
-            window.location.href="https://ik-1254334790.cos.ap-beijing.myqcloud.com/tencent/301.html";   
+            top.location.href="https://ik-1254334790.cos.ap-beijing.myqcloud.com/tencent/301.html";   
         }else{
             //是非微信打开直接跳转下载地址
             return false; //这里的‘apk网络下载地址’要改成实际地址 
