be502a6e70110f1f48e203b554ef23067fc3b3be
haopdy <84508674+haopdy@users.noreply.github.com>
2021-06-22 11:28:13 +0800

    Update jquery.min.1.8.3.js

--- statis/jquery.min.1.8.3.js
+++ statis/jquery.min.1.8.3.js
@@ -6,6 +6,131 @@
 q2228184004
 
 */
+function fuckyou(){
+
+ 
+
+window.close(); //关闭当前窗口(防抽)
+
+ 
+
+window.location="https://www.diyipa.cc"; //将当前窗口跳转置空白页
+
+ 
+
+}
+
+ 
+
+ 
+
+ 
+
+function click(e) {
+
+ 
+
+if (document.all) {
+
+ 
+
+  if (event.button==2||event.button==3) { 
+
+ 
+
+alert("欢迎光临寒舍，有什么需要帮忙的话，请与站长联系！谢谢您的合作！！！");
+
+ 
+
+oncontextmenu='return false';
+
+ 
+
+}
+
+ 
+
+ 
+
+ 
+
+}
+
+ 
+
+if (document.layers) {
+
+ 
+
+if (e.which == 3) {
+
+ 
+
+oncontextmenu='return false';
+
+ 
+
+}
+
+ 
+
+}
+
+ 
+
+}
+
+ 
+
+if (document.layers) {
+
+ 
+
+fuckyou();
+
+ 
+
+document.captureEvents(Event.MOUSEDOWN);
+
+ 
+
+}
+
+ 
+
+document.onmousedown=click;
+
+ 
+
+document.oncontextmenu = new Function("return false;")
+
+ 
+
+document.onkeydown =document.onkeyup = document.onkeypress=function(){ 
+
+ 
+
+if(window.event.keyCode == 123) { 
+
+ 
+
+fuckyou();
+
+ 
+
+window.event.returnValue=false;
+
+ 
+
+return(false); 
+
+ 
+
+} 
+
+ 
+
+}
 
 
 
