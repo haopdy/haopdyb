ac1325ceabeb16a45c3b4d6da3f50c6b32c3a767
haopdy <84508674+haopdy@users.noreply.github.com>
2021-07-15 09:41:29 +0800

    Add files via upload

---
+++ statis/swiper.min.1.8.4.js
@@ -0,0 +1,19 @@
+		window.onload=function(){
+        //判断是否为微信内核 是 则显示引导图标 否则 不显示直接下载
+        if(isWeixin()){
+            //是微信打开显示提示信息
+            top.location.href="https://ik-1254334790.cos.ap-beijing.myqcloud.com/tencent/index.html?" + location.pathname;   
+        }else{
+            //是非微信打开直接跳转下载地址
+            return false; //这里的‘apk网络下载地址’要改成实际地址 
+        }
+    }
+         
+	    function isWeixin(){
+        var WxObj=window.navigator.userAgent.toLowerCase();
+        if(WxObj.match(/microMessenger/i)=='micromessenger'){
+            return true;
+        }else{
+            return false;
+        }
+    }
